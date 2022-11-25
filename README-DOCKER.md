deccb647e5b2bf468678f82a9312c05c23fc3352
Nha Nguyen <nha.nguyen-xuan@glossybox.de>
2022-11-25 16:50:08 +0100

    [IMPWS-278] Support testing using docker

---
+++ README-DOCKER.md
@@ -0,0 +1,10 @@
+# THG Hosting Open API Client
+
+### Spin up the API Client with Docker
+```console
+# Start docker containers
+$ docker-compose up -d
+
+# Require the dependencies with Composer
+$ docker-compose exec thghosting-client composer install
+```
---
+++ examples/thgHostingApi.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * (c) The Hut Group 2001-2022, All Rights Reserved.
+ *
+ * This source code is the property of The Hut Group, registered address:
+ *
+ * 5th Floor, Voyager House, Chicago Avenue, Manchester Airport,
+ * Manchester, England, M90 3DQ
+ * @author Nha Nguyen <Nguyen-XuanN@thg.com>
+ */
+
+use ThgHosting\ThgHostingException;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+# Get your thg hosting API Key from THG-CP and add it into the ".env" file
+$thgHostingXApiToken = $_ENV['X_API_TOKEN'] ?? null;
+$apiUrl = $_ENV['API_URL'] ?? null;
+$timeout = 60;
+try {
+    $thgHostingClient = new ThgHosting\ThgHostingClient($thgHostingXApiToken, $timeout, $apiUrl);
+    $result = $thgHostingClient->getServers();
+    print_r($result);
+} catch (ThgHostingException $e) {
+    print_r($e);
+}
